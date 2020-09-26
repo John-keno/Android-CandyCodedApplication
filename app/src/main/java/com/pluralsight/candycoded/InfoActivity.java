1594d8e79c59c913d97b84a67b077395fba0e504
John Keno <iwehjohn@gmail.com>
2020-09-26 03:38:07 +0100

    updated commit Start the Phone Activity

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -26,9 +26,6 @@ public class InfoActivity extends AppCompatActivity {
 
     }
 
-    // ***
-    // TODO - Task 2 - Launch the Google Maps Activity
-    // ***
     public void createMapIntent(View view){
         Uri uri = Uri.parse("geo:0,0?q=618 E South St Orlando, FL 32801");
         Intent mapIntent = new Intent(Intent.ACTION_VIEW,uri);
@@ -38,13 +35,16 @@ public class InfoActivity extends AppCompatActivity {
         }
         else {
             Toast.makeText
-                    (getApplicationContext(),"please install google maps view",
+                    (getApplicationContext(),"Please install google maps to view",
                             Toast.LENGTH_LONG).show();
         }
 
     }
 
-    // ***
-    // TODO - Task 3 - Launch the Phone Activity
-    // ***
+    public void createPhoneIntent(View view){
+        Uri uri = Uri.parse("tel:0123456789");
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        intent.setData(uri);
+        startActivity(intent);
+    }
 }
